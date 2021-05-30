d88eed0cff2544076b843f28ae8602f4528604bc
benson odongo <b2013odongo@outlook.com>
2021-05-30 22:18:52 +0300

    library , consoleapp and tests

---
+++ EmployeeHierarchy/Program.cs
@@ -0,0 +1,30 @@
+﻿using EmployeesLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EmployeeHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees employees = new Employees(
+                "Employee4,Employee2,500" +
+                "\n" +
+                "Employee3,Employee1,800" +
+                "\n" +
+                "Employee1,,1000" +
+                "\n" +
+                "Employee5,Employee1,500" +  
+                "\n" +
+                "Employee2,Employee1,500" 
+                );
+
+            int salary = employees.managerSalaryBudget("Employee1");
+
+            Console.WriteLine(salary);
+        }
+
+    }
+}
---
+++ UnitTestEmployeesLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestEmployeesLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestEmployeesLibrary")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c76e969d-31ef-460f-9a5a-182fff9ddb8a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
